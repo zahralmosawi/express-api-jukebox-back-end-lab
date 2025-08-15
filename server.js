a5a9d9e0ef77e5db58f6342a0ec517dbaa71368c
zahralmosawi <zalmosawii65@gmail.com>
2025-08-15 18:20:11 +0300

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('morgan')
 const trackRoutes = require('./routes/trackRoutes')
+const cors = require('cors')
 
 const dotenv = require('dotenv')
 dotenv.config()
@@ -13,6 +14,7 @@ mongoose.connection.on('connected', () => {
     console.log('connected to database')
 })
 
+app.use(cors({origin: 'http://localhost:5173'}))
 app.use(express.json())
 app.use(logger('dev'))
 app.use('/', trackRoutes)
