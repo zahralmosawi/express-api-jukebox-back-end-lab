a322eaa6fb5d046542562737e6396c2ca84e0c67
zahralmosawi <zalmosawii65@gmail.com>
2025-08-16 16:22:08 +0300

    updated routes

--- routes/trackRoutes.js
+++ routes/trackRoutes.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const trackController = require('../controllers/track')
 
-router.post('/tracks', trackController.createTrack)
+router.post('/add-track', trackController.createTrack)
 router.get('/tracks', trackController.listTrack)
 router.get('/tracks/:id', trackController.trackDetails)
-router.put('/tracks/:id', trackController.updateTrack)
-router.delete('/tracks/:id', trackController.deleteTrack)
+router.put('/edit-track/:id', trackController.updateTrack)
+router.delete('/:id', trackController.deleteTrack)
 
 module.exports = router
